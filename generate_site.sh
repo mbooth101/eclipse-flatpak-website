f017ab9ebe52b21f4b8bb76dbdf93fa47cecbf5e
Mat Booth <mbooth@apache.org>
2020-01-23 09:57:52 +0000

    Update version of stats generator

--- generate_site.sh
+++ generate_site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p site/charts
-python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph --model-skip-today \
-	--output site/charts/downloads.png \
+python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph \
+	--model-skip-today --start-date 2019-11-18 --output site/charts/downloads.png \
 	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Committers
