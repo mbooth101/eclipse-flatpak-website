7f9d410c1ad2ec99c4c1f29fa1394ebe2b021847
Mat Booth <mbooth@apache.org>
2020-01-23 14:00:58 +0000

    First stab at showing monthly download stats

--- generate_site.sh
+++ generate_site.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir -p site/charts
+
 python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph \
 	--model-skip-today --start-date 2019-11-18 --output site/charts/downloads.png \
+	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Committers
+
+python3 flathub-api-stats-generator/main.py --report monthly_downloads_by_app --cache-dir stats_cache --type data \
+	--model-skip-today --start-date 2019-11-18 --output site/charts/monthly_downloads.json \
 	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Committers
