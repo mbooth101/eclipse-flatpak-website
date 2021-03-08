ccf061e6fecf8530ab1a2f48b2afb9afde233d63
Mat Booth <mat.booth@redhat.com>
2021-03-08 14:48:29 +0000

    Add javascript distro to charts
    Signed-off-by: Mat Booth <mat.booth@redhat.com>

--- generate_site.sh
+++ generate_site.sh
@@ -4,16 +4,16 @@ mkdir -p site/charts
 
 python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph \
 	--model-skip-today --start-date 2019-11-11 --output site/charts/downloads_daily.png \
-	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Committers
+	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Javascript,app-id=org.eclipse.Committers
 
 python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph \
 	--model-skip-today --start-date 2019-11-11 --output site/charts/downloads_weekly.png \
-	--report-args period=weekly,app-id=org.eclipse.Java,app-id=org.eclipse.Committers
+	--report-args period=weekly,app-id=org.eclipse.Java,app-id=org.eclipse.Javascript,app-id=org.eclipse.Committers
 
 python3 flathub-api-stats-generator/main.py --report downloads_by_app --cache-dir stats_cache --type graph \
 	--model-skip-today --start-date 2019-11-11 --output site/charts/downloads_monthly.png \
-	--report-args period=monthly,app-id=org.eclipse.Java,app-id=org.eclipse.Committers
+	--report-args period=monthly,app-id=org.eclipse.Java,app-id=org.eclipse.Javascript,app-id=org.eclipse.Committers
 
 python3 flathub-api-stats-generator/main.py --report monthly_downloads_by_app --cache-dir stats_cache --type data \
 	--model-skip-today --start-date 2019-11-11 --output site/charts/monthly_downloads.json \
-	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Committers
+	--report-args app-id=org.eclipse.Java,app-id=org.eclipse.Javascript,app-id=org.eclipse.Committers
